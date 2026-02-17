0bbeed2aa27c81ceb0284b150c6ddd0a771601e7
prittk <prittk@my.erau.edu>
2026-02-16 22:19:48 -0500

    update to Movement adding clamp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     Rigidbody rb;
     Vector2 movement;
+
+    [SerializeField]float clampedX = 4.25f;
+    [SerializeField] float clampedZ = 3f;
+
     [SerializeField] float moveSpeed = 5f;
     void Awake()
     {
@@ -30,8 +34,12 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 currentPos = rb.position;
         Vector3 moveDir = currentPos + direction * (moveSpeed *Time.fixedDeltaTime);
+        moveDir = new Vector3(Mathf.Clamp(moveDir.x, -clampedX, clampedX), moveDir.y, Mathf.Clamp(moveDir.z, -clampedZ, clampedZ));
+
         rb.MovePosition(moveDir);
     }
+
+   
 
     
 
