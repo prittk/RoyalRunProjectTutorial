b3f46da1886a457ca03651059ece28cf374b4137
prittk <prittk@my.erau.edu>
2026-02-16 19:24:37 -0500

    Fixed Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
-using UnityEditor.Callbacks;
-using UnityEditor.Rendering.LookDev;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,30 +9,28 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     Rigidbody rb;
     Vector2 movement;
-    [SerializeField] float moveSpeed = 10f;
-    void Start()
+    [SerializeField] float moveSpeed = 5f;
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
     void FixedUpdate()
     {
-        MovePlayer(new Vector3(movement.x, 0 , movement.y));
+        MovePlayer(new Vector3(movement.x, 0f , movement.y));
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        while (context.performed)
-        {
-        movement = context.ReadValue<Vector2>();
-        }
         
-        movement = Vector2.zero;
+        movement = context.ReadValue<Vector2>();
         Debug.Log(movement);
     }
 
     public void MovePlayer(Vector3 direction)
     {
-        rb.AddForce(direction * moveSpeed *Time.deltaTime);
+        Vector3 currentPos = rb.position;
+        Vector3 moveDir = currentPos + direction * (moveSpeed *Time.fixedDeltaTime);
+        rb.MovePosition(moveDir);
     }
 
     
