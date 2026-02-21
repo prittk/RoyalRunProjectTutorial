fcfd2db90b56d20bd8c58afd6fb726dfb36d7ba3
prittk <prittk@my.erau.edu>
2026-02-20 20:09:52 -0500

    Change obstacle group[] to be handled by unity

--- Assets/Prefab/ObstacleSpawner.cs
+++ Assets/Prefab/ObstacleSpawner.cs
@@ -5,7 +5,7 @@ using Unity.VisualScripting;
 public class ObstacleSpawner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] GameObject[] obstacle = new GameObject[2];
+    [SerializeField] GameObject[] obstacle;
     [SerializeField] Transform obstacleGroup;
 
 
