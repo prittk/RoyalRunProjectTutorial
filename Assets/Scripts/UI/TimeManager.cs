f4de8cb2f986f3a5d235fa930f946e82c3a8fb42
prittk <prittk@my.erau.edu>
2026-03-19 20:28:45 -0400

    Added GameOver Restart

---
+++ Assets/Scripts/UI/TimeManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float startTime = 10f;
+    [SerializeField] float resetTimer = 5f;
+
+    [SerializeField] PlayerController playerCont;
+    private bool isOver = false;
+
+    private float timeLeft = 0f;
+    [SerializeField] TMP_Text tmp;
+    [SerializeField] GameObject gameOver;
+ 
+    // Update is called once per frame
+    void Start()
+    {
+        Time.timeScale = 1f;
+        timeLeft = startTime;
+    }
+    void Update()
+    {
+        if(isOver) return;
+        
+        timeLeft -= Time.deltaTime;
+
+        tmp.text = "00:00:"+timeLeft.ToString("F1");
+
+        if (timeLeft <= 0) StartCoroutine(GameOver());
+       
+    }
+
+    IEnumerator GameOver()
+    {
+
+        isOver = true;
+
+        playerCont.enabled=false;
+
+        gameOver.SetActive(true);
+        Time.timeScale = .1f;//Slows down game while
+
+        yield return new WaitForSeconds(.5f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        
+
+
+    }
+}
